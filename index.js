8638968cdf26d8bd75c66ae6cc445bb7921f65bb
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-04 19:35:01 -0300

    feat: add getUserByEmailRepository

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import "dotenv/config.js";
 import express from "express";
 import { CreateUserController } from "./src/controllers/create-user.js";
+import { GetUserByIdController } from "./src/controllers/get-user-by-id.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/users', async (request, response) => {
-  const createdUserController = new CreateUserController()
+app.post("/api/users", async (request, response) => {
+  const createdUserController = new CreateUserController();
 
-  const {statusCode, body} = await createdUserController.execute(request)
+  const { statusCode, body } = await createdUserController.execute(request);
 
   response.status(statusCode).send(body);
-})
+});
 
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+app.get("/api/users/:userId", async (request, response) => {
+  const getUserByIdController = new GetUserByIdController();
+
+  const { statusCode, body } = await getUserByIdController.execute(request);
+
+  response.status(statusCode).send(body);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);

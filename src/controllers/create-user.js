b025a39427af0ad49e3f89a9db20529dc5f3262f
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-04 20:30:36 -0300

    feat: ensure email already in user error is used

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,6 +1,7 @@
 import { CreateUserUseCase } from "../use-cases/create-user.js";
 import validator from "validator";
 import { badRequest, created, serverError } from "./helpers.js";
+import { EmailAlreadyInUseError } from "../errors/user.js";
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -36,11 +37,15 @@ export class CreateUserController {
 
       const createdUser = await createUserUseCase.execute(params);
 
-      return created(createdUser)
+      return created(createdUser);
       //retornar status HTTP
     } catch (error) {
+      if (error instanceof EmailAlreadyInUseError) {
+        return badRequest({ message: error.message });
+      }
+
       console.log(error);
-      return serverError()
+      return serverError();
     }
   }
 }
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,18 +2,19 @@ import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 import { PostgresCreateUserRepository } from "../repositories/postgres/create-user.js";
 import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { EmailAlreadyInUseError } from "../errors/user.js";
 
 export class CreateUserUseCase {
   async execute(createUserParams) {
     //TODO: verificar se o emial ja esta em uso
-    const postgresGetUserByEmail = new PostgresGetUserByEmailRepository()
+    const postgresGetUserByEmail = new PostgresGetUserByEmailRepository();
 
     const userWithProvidedEmail = await postgresGetUserByEmail.execute(
       createUserParams.email
-    )
+    );
 
     if (userWithProvidedEmail) {
-      throw new Error('The provided e-mail is already in use!')
+      throw new EmailAlreadyInUseError();
     }
 
     //gerar ID do usuario

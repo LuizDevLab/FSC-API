989215db36d4ccff61a57c41131ab035c333aa1d
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-04 19:34:39 -0300

    feat: add validations on controller

---
+++ src/controllers/get-user-by-id.js
@@ -0,0 +1,20 @@
+import { GetUserByIdUseCase } from "../use-cases/get-user-by-id.js";
+import { badRequest, created, serverError } from "./helpers.js";
+
+export class GetUserByIdController {
+  async execute(httpRequest) {
+    try {
+      const getUserByIdUseCase = new GetUserByIdUseCase();
+
+      const user = await getUserByIdUseCase.execute(httpRequest.params.userId);
+
+      return {
+        statusCode: 200,
+        body: user,
+      };
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+}

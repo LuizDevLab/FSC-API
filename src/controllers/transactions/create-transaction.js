9fb73e87624ed0ffe4e26c3e0bcd3f16a0c00323
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-25 21:08:43 -0300

    feat: add transaction helpers function

--- src/controllers/transactions/create-transaction.js
+++ src/controllers/transactions/create-transaction.js
@@ -6,6 +6,7 @@ import {
   requiredFieldIsMissingResponse,
   validateRequiredFields,
 } from "../helpers/validation.js";
+import { checkIfAmountIsValid, checkIfTypeIsValid, invalidAmountResponse, invalidTypeResponse } from "../helpers/transaction.js";
 
 export class CreateTransactionController {
   constructor(createTransactionUseCase) {
@@ -34,26 +35,18 @@ export class CreateTransactionController {
         return invalidIdResponse();
       }
 
-      const amountIsValid = validator.isCurrency(params.amount.toString(), {
-        digits_after_decimal: [2],
-        allow_negatives: false,
-        decimal_separator: ".",
-      });
+      const amountIsValid = checkIfAmountIsValid(params.amount)
 
       if (!amountIsValid) {
-        return badRequest({
-          message: "The amount must be a valid currency.",
-        });
+        return invalidAmountResponse()
       }
 
       const type = params.type.trim().toUpperCase();
 
-      const typeIsValid = ["EARNING", "EXPENSE", "INVESTMENT"].includes(type);
+      const typeIsValid = checkIfTypeIsValid(type)
 
       if (!typeIsValid) {
-        return badRequest({
-          message: "the type must be EARNING, EXPENSE or INVESTMENT.",
-        });
+        return invalidTypeResponse()
       }
 
       const transaction = await this.createTransactionUseCase.execute({

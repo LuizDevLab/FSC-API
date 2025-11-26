499ae6fe417af27bfe673f784e80f460fb83d9fe
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-25 21:38:55 -0300

    fix: ensure getTransactionsbyid throws new Error

---
+++ src/use-cases/transactions/get-transactions-by-user-id.js
@@ -0,0 +1,24 @@
+import { UserNotFoundError } from "../../errors/user.js";
+
+export class GetTransactionsByUserIdUseCase {
+  constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
+    this.getTransactionsByUserIdRepository = getTransactionsByUserIdRepository;
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+
+  async execute(params) {
+    // validar se o usuario existe
+    const user = await this.getUserByIdRepository.execute(params.userId);
+
+    if (!user) {
+      throw new UserNotFoundError(params.userId)
+    }
+
+    // chamar o repository
+    const transactions = await this.getTransactionsByUserIdRepository.execute(
+      params.userId
+    );
+
+    return transactions;
+  }
+}

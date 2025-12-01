f991239a4f832a2ad07e23fcbbcabd6d11351981
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-11-30 21:07:17 -0300

    feat: add delete transaction controller

---
+++ src/controllers/transactions/delete-transaction.js
@@ -0,0 +1,27 @@
+import { ok, serverError } from "../helpers/http.js";
+import { checkIfIdIsValid, invalidIdResponse } from "../helpers/validation.js";
+
+export class DeleteTransactionUseCase {
+  constructor(deleteTransactionUseCase) {
+    this.deleteTransactionUseCase = deleteTransactionUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId);
+
+      if (!idIsValid) {
+        return invalidIdResponse();
+      }
+
+      const transaction = await this.deleteTransactionUseCase.execute(
+        httpRequest.params.transactionId
+      );
+
+      return ok(transaction)
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
+}

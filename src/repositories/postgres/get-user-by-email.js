1e5e393e53c27a9b55b3a57a5b81671026634f1b
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-12-08 22:37:06 -0300

    feat: ensure get user by email use prisma

--- src/repositories/postgres/get-user-by-email.js
+++ src/repositories/postgres/get-user-by-email.js
@@ -1,12 +1,12 @@
+import { prisma } from "../../../prisma/prisma.js";
 import { PostgresHelper } from "../../db/postgres/helper.js";
 
 export class PostgresGetUserByEmailRepository {
   async execute(email) {
-    const user = await PostgresHelper.query(
-      'SELECT * FROM users WHERE email = $1',
-      [email]
-    )
-
-    return user[0]
+    return await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
   }
 }

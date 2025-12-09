733bf7a33e95eff05b1c571804a4653ac0b54c7c
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2025-12-08 22:46:29 -0300

    feat: ensure DeleteUser and GetUserById use prisma

--- src/repositories/postgres/delete-user.js
+++ src/repositories/postgres/delete-user.js
@@ -1,12 +1,12 @@
+import { prisma } from "../../../prisma/prisma.js";
 import { PostgresHelper } from "../../db/postgres/helper.js";
 
 export class PostgresDeleteUserRepository {
   async execute(userId) {
-    const deletedUser = await PostgresHelper.query(
-      "DELETE FROM users WHERE id = $1 RETURNING *",
-      [userId]
-    );
-
-    return deletedUser[0];
+    return await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
   }
 }
--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,12 +1,12 @@
+import { prisma } from "../../../prisma/prisma.js";
 import { PostgresHelper } from "../../db/postgres/helper.js";
 
 export class PostgresGetUserById {
   async execute(userId) {
-    const user = await PostgresHelper.query(
-      'SELECT * FROM users WHERE id = $1',
-      [userId]
-    )
-
-    return user[0]
+    return await prisma.user.findUnique({
+      where:{
+        id: userId
+      }
+    })
   }
 }

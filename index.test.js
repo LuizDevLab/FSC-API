92a2171f5732e366bb7ae19a1b750263d1d980b4
Luiz Henrique da Silva Ramos <luizgthub@gmail.com>
2026-03-22 00:01:23 -0300

    chore: initial jest setup

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+describe("sum function", () => {
+  it("should sum two numbers correctly", () => {
+    //arrange
+    const a = 2;
+    const b = 2;
+
+    //act
+    const result = sum(a, b)
+
+    //assert
+    expect(result).toBe(4)
+  });
+});
